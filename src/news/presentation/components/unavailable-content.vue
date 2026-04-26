6522d0e4267e6f10de905b066fbfc427d10e68bf
josefloresingaruca <josefloresingaruca@gmail.com>
2026-04-25 19:03:11 -0500

    first commit- api news

---
+++ src/news/presentation/components/unavailable-content.vue
@@ -0,0 +1,32 @@
+<script setup lang="js">
+  import {useI18n} from "vue-i18n";
+  import {toRefs} from "vue";
+
+  /**
+   * Presentation fallback component for infrastructure/application errors.
+   */
+
+  /**
+   * @typedef {Object} UnavailableContentProps
+   * @property {Array<unknown>} errors
+   */
+
+  const { t } = useI18n();
+
+  /** @type {UnavailableContentProps} */
+  const props = defineProps({ errors: null});
+  const { errors } = toRefs(props);
+</script>
+
+<template>
+  <div>
+    <div><h4>{{ t('unavailable-news') }}</h4></div>
+    <div v-for="error in errors">
+      <h6>{{ error }}</h6>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/shared/presentation/components/language-switcher.vue
@@ -0,0 +1,17 @@
+<script setup lang="js">
+/**
+ * Presentation component that exposes available locales from vue-i18n.
+ */
+</script>
+
+<template>
+  <pv-select-button v-model="$i18n.locale" :options="$i18n.availableLocales">
+    <template #option="slotProps">
+      <span>{{ slotProps.option.toUpperCase() }}</span>
+    </template>
+  </pv-select-button>
+</template>
+
+<style scoped>
+
+</style>
